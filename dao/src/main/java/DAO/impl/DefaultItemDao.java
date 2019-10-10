d389f1c6050ccdb4a85e1fcf33b9bcc21c9a39da
tanya_melgui <Amstgirl@gmail.com>
2019-10-10 10:57:49 +0300

    Item, ItemDao, DefaultItemDao added created table for project

---
+++ dao/src/main/java/DAO/impl/DefaultItemDao.java
@@ -0,0 +1,57 @@
+package DAO.impl;
+
+import DAO.ItemDao;
+import Model.AuthUser;
+import Model.Item;
+import com.github.JDBCConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DefaultItemDao implements ItemDao {
+
+    private static volatile ItemDao instance;
+
+    public static ItemDao getInstance() {
+        ItemDao localInstance = instance;
+        if (localInstance == null) {
+            synchronized (ItemDao.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new DefaultItemDao();
+                }
+            }
+        }
+        return localInstance;
+    }
+
+
+    public Item getItemByName(String itemName) {
+
+        try (Connection connection = JDBCConnection.connect(); PreparedStatement preparedStatement = connection.prepareStatement("select * from item where item_name = ?")) {
+            preparedStatement.setString(1, itemName);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                Long item_id = resultSet.getLong("item_id");
+                String item_name = resultSet.getString("item_name");
+                String item_description= resultSet.getString("item_description");
+                long item_quantity = resultSet.getLong("item_quantity");
+                int item_category_id = resultSet.getInt("item_category_id ");
+                long price_for_one = resultSet.getLong("price_for_one");
+                //Item item = new Item();
+                return new Item(item_id,item_name,item_description,item_quantity,item_category_id,price_for_one);
+            }
+            else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
+
+
---
+++ dao/src/main/java/Model/Item.java
@@ -0,0 +1,76 @@
+package Model;
+
+
+public class Item {
+    private Long id;
+    private String itemName;
+    private String itemDescription;
+    private long itemQuantity;
+    private int itemCategoryId;
+    private long priceForOne;
+
+    public Item() {
+    }
+
+    public Item(Long id, String itemName, String itemDescription, long itemQuantity, int itemCategoryId, long priceForOne) {
+        this.id = id;
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.itemQuantity = itemQuantity;
+        this.itemCategoryId = itemCategoryId;
+        this.priceForOne = priceForOne;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public long getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(long itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public int getItemCategoryId() {
+        return itemCategoryId;
+    }
+
+    public void setItemCategoryId(int itemCategoryId) {
+        this.itemCategoryId = itemCategoryId;
+    }
+
+    public long getPriceForOne() {
+        return priceForOne;
+    }
+
+    public void setPriceForOne(long priceForOne) {
+        this.priceForOne = priceForOne;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "id=" + id + ", itemName='" + itemName + '\'' + ", itemDescription='" + itemDescription + '\'' + ", itemQuantity=" + itemQuantity + ", itemCategoryId=" + itemCategoryId + ", priceForOne=" + priceForOne + '}';
+    }
+}
